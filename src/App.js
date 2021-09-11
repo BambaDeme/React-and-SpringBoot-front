557336e897ab1ed9a3f42e199fd189c8d6c8bcf4
bambademe <cheikhbambademe@gmail.com>
2021-09-11 09:56:09 -0700

    link front to API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { getAllStudents } from "./client";
+
+import { Table } from "antd";
+class App extends React.Component {
+  state = {
+    students: [],
+  };
+
+  componentDidMount() {
+    this.fetchAllStudents();
+  }
+  fetchAllStudents() {
+    getAllStudents().then((res) =>
+      res.json().then((students) => {
+        console.log(students);
+        this.setState({
+          students: students,
+        });
+      })
+    );
+  }
+
+  render() {
+    const columns = [
+      {
+        title: "Student Id",
+        dataIndex: "studentId",
+        key: "studentId",
+      },
+      {
+        title: "First Name",
+        dataIndex: "firstName",
+        key: "firstName",
+      },
+      {
+        title: "Last Name",
+        dataIndex: "lastName",
+        key: "lastName",
+      },
+      {
+        title: "Email",
+        dataIndex: "email",
+        key: "email",
+      },
+      {
+        title: "Gender",
+        dataIndex: "gender",
+        key: "gender",
+      },
+    ];
+    return (
+      <>
+        <h1>Liste of students from spring api</h1>
+        {this.state.students && this.state.students.length ? (
+          <Table
+            dataSource={this.state.students}
+            columns={columns}
+            rowKey="studentId"
+          />
+        ) : (
+          <h3>No students found</h3>
+        )}
+      </>
+    );
+  }
 }
 
 export default App;
